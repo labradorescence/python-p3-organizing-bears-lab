beda22ed19c94b0b3b8f14ccb4fd49a68124fb67
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-01-31 16:37:36 -0500

    prac

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    age INT, 
+    sex CHAR(1), 
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (
+    id,  
+    name,  
+    age,  
+    sex,  
+    color, 
+    temperament, 
+    alive)
+VALUES 
+(1, "Mr. Chocolate", 3, "F", "Orange", "Calm", true),
+(2, "Rowdy", 3, "F", "Orange", "Calm", true),
+(3, "Tabitha", 3, "F", "Orange", "Calm", true),
+(4, "Sergeant Brown", 3, "F", "Orange", "Calm", true),
+(5, "Melissa", 3, "F", "Orange", "Calm", true),
+(6, "Grinch", 3, "F", "Orange", "Calm", true),
+(7, "Wendy", 3, "F", "Orange", "Calm", true),
+(8, null, 3, "F", "Orange", "Calm", true)
